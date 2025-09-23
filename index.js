4f5fd0990c00ae5e55395463dae0436c854f5bdc
24ky <zakaria.lesbat87@gmail.com>
2025-09-23 22:36:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,9 +12,12 @@ import admin from "firebase-admin";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync(path.join(__dirname, "firebase-service-account.json"), "utf8")
-);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
 
 const app = express();
 app.use(cors());
