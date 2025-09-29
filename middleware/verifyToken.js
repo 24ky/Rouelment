a244d019ed5e40cdefeea78d32fe9af1e0e3ec9f
24ky <zakaria.lesbat87@gmail.com>
2025-09-29 13:34:14 +0100

    Create verifyToken.js

---
+++ middleware/verifyToken.js
@@ -0,0 +1,20 @@
+// middleware/verifyToken.js
+import admin from "../firebase.js";
+
+export async function verifyToken(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ error: "Token manquant" });
+  }
+
+  const idToken = authHeader.split("Bearer ")[1];
+
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(idToken);
+    req.user = decodedToken; // on attache le user à la req
+    next();
+  } catch (error) {
+    return res.status(401).json({ error: "Token invalide" });
+  }
+}
