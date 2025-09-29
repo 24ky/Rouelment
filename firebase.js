9c6ac21ce0455a5aca82fd631d1f99e237b1ce22
24ky <zakaria.lesbat87@gmail.com>
2025-09-29 14:07:26 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,10 @@
-// firebase.js
 import admin from "firebase-admin";
 import { getApps } from "firebase-admin/app";
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 if (serviceAccount.private_key) {
+  // Remplace les \n littéraux par de vrais retours à la ligne dans la clé privée
   serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
 }
 
